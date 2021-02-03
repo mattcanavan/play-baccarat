a5feec5bf1405d0ec37b9715b028c75b7d2c80ff
Matt Canavan <mattcanavan@users.noreply.github.com>
2021-02-03 11:32:38 -0800

    Rewriting array that stores hand

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 import Deck from "./deck.js";
 
 // GLOBAL variables
-let readyForNewGame = true;
 let deck;
 const CARD_VALUE_MAP = {
   A: 1,
@@ -28,13 +27,7 @@ const playerSlotOne = document.querySelector(".player-card-slot-one");
 const playerSlotTwo = document.querySelector(".player-card-slot-two");
 const playerSlotThree = document.querySelector(".player-card-slot-three");
 
-// EVENT Listeners
-
-// window.onload = (event) => {
-//     // assemble deck when page loads
-//     assembleShoe()
-// }
-
+// EVENT Listener
 document.addEventListener("click", runGame);
 
 // RUN GAME
@@ -44,53 +37,96 @@ function runGame() {
   deck.shuffle();
 
   cleanGameBoard();
-  dealCards().then((obj) => {
-    //   initial vals
-    const playerThirdCard = [];
-    const bankerThirdCard = [];
+  dealCards()
+    .then((obj) => {
+      //   initial vals
+      const playerThirdCard = [];
+      const bankerThirdCard = [];
 
-    // Count hand totals with reducer, where 0 is the initial value
-    const playerCount = obj.player.reduce((acc, value) => {
-      const val = CARD_VALUE_MAP[value.value] % 10;
-      return acc + val;
-    }, 0);
-    const bankerCount = obj.banker.reduce((acc, value) => {
-      const val = CARD_VALUE_MAP[value.value] % 10;
-      return acc + val;
-    }, 0);
+      // Count hand totals with reducer, where 0 is the initial value
+      const playerCount = obj.player.reduce((acc, value) => {
+        const val = CARD_VALUE_MAP[value.value];
+        return (acc + val) % 10;
+      }, 0);
+      const bankerCount = obj.banker.reduce((acc, value) => {
+        const val = CARD_VALUE_MAP[value.value];
+        return (acc + val) % 10;
+      }, 0);
 
-    // Step through hands https://www.onlinecasinoselite.org/getting-started/gambling-rules/baccarat
-
-    // if(playerCount === 8)
-
-    if (playerCount < 6){
-        playerThirdCard.push(deck.draw())
-    }
-
-    if (bankerCount < 7) {
-        if(bankerCount === 0 || bankerCount === 1 || bankerCount === 2){
-            bankerThirdCard.push(deck.draw())
-        } else if (bankerCount === 3 || bankerCount === 6){
-            
-            // if play doesnt have third card
-            if (!playerThirdCard.length) {
-            bankerThirdCard.push(deck.draw())
+      // Step through hands https://www.onlinecasinoselite.org/getting-started/gambling-rules/baccarat
+      if (
+        playerCount === 8 ||
+        playerCount === 9 ||
+        bankerCount === 8 ||
+        bankerCount === 9
+      ) {
+        if (playerCount === bankerCount) {
+          return textArea.innerText = "Draw"
+        } else {
+          return playerCount < bankerCount ? textArea.innerText = "Banker Wins" : textArea.innerText = "Player Wins"
         }
-    }
+      }
 
-    // if play doesnt have third card
-    if (!playerThirdCard.length) {
-        bankerThirdCard.push(deck.draw())
-    } else {
-        
-    }
+      // PLAYER
+      if (playerCount <= 5) {
+        playerThirdCard.push(deck.draw());
+        playerSlotThree.appendChild(playerThirdCard[0].getHTML())
+      } else{
+        // stand
+      }
 
+      // BANKER
+      if (bankerCount <= 7) {
+        if (bankerCount === 0 || bankerCount === 1 || bankerCount === 2) {
+          // must draw
+          bankerThirdCard.push(deck.draw());
+        } else if (
+          bankerCount === 3 ||
+          bankerCount === 4 ||
+          bankerCount === 5 ||
+          bankerCount === 6
+        ) {
+          // depends on player's 3rd card
+          if (!playerThirdCard.length) {
+            bankerThirdCard.push(deck.draw());
+            bankerSlotThree.appendChild(bankerThirdCard[0].getHTML())
+          } else{
+            // stand
+          }
+        }
+      }
 
+      // FINAL count
+      const playerFinalCount = playerThirdCard.reduce((acc, value) => {
+        if(!value){
+          return playerCount
+        } else{
+          const val = parseInt(CARD_VALUE_MAP[value.value]) + playerCount;
+          return (acc + val) % 10;
+        }
+      }, 0);
+      const bankerFinalCount = bankerThirdCard.reduce((acc, value) => {
+        if(!value){
+          return bankerCount
+        } else{
+          const val = CARD_VALUE_MAP[value.value] + bankerCount;
+          debugger
+          return (acc + val) % 10;
+        }
+      }, 0);
 
+      console.log(playerFinalCount, bankerFinalCount)
 
-
-    console.log(obj)
-  });
+      // FINAL comparison
+      if (playerFinalCount === bankerFinalCount){
+        return textArea.innerText = "Draw"
+      } else{
+        return playerFinalCount < bankerFinalCount ? textArea.innerText = "Banker Wins" : textArea.innerText = "Player Wins"
+      }
+    })
+    .then((results) => {
+      return console.log(results);
+    });
 }
 
 // HELPER FUNCTIONS
@@ -123,66 +159,7 @@ function dealCards() {
     playerSlotTwo.appendChild(playerCards[1].getHTML());
     bankerSlotTwo.appendChild(bankerCards[1].getHTML());
 
-    // Count hand totals with reducer
-    const playerCount = playerCards.reduce((acc, value) => {
-      const val = CARD_VALUE_MAP[value.value] % 10;
-      return acc + val;
-    }, 0); //where 0 is the initial value
-
-    const bankerCount = bankerCards.reduce((acc, value) => {
-      const val = CARD_VALUE_MAP[value.value] % 10;
-      return acc + val;
-    }, 0); //where 0 is the initial value
-
-    // resolve counts
+    // resolve cards
     resolve({ player: playerCards, banker: bankerCards });
   })};
 
-// // GAME Functions
-
-// // STEP 5: All those hand conditions
-// function handConitions(playerIn, bankerIn) {
-//     // inital vals
-//     let playerScoreDone = false;
-//     let bankerScoreDone = false;
-
-//     let pScore = 0;
-//     let bScore = 0;
-
-//     console.log(playIn)
-
-//     while (!playerScoreDone && !bankerScoreDone ) {
-//         // the hands play off each other
-
-//         // one
-//         if (playerIn > 6){
-//             return pScore = playerIn;
-//         } else {
-//             // less-than equal to five
-
-//         }
-
-//         // two
-//         if (bankerIn>){
-
-//         }
-
-//     }
-//     const bankerFinal = 0;
-
-//     console.log(playerIn)
-//     // If either the player or banker is dealt a total of eight or nine, both the player and banker stand.
-//     if (playerIn = 9){
-
-//     }
-
-// source: https://www.caesars.com/casino-gaming-blog/latest-posts/table-games/baccarat/how-to-play-baccarat
-// (1) When the cards dealt are greater than nine, you have to add the two together and drop the one (or two) to get the value.
-// done
-
-// If the player’s total is five or less, then the player will receive another card. Otherwise, the player will stand.
-// If the player stands, then the banker hits on a total of 5 or less.
-// The final betting option, a tie, pays out 8-to-1. Conveniently, there are also sheets at the table for you to keep track of your score.
-
-// Look for pairs?
-// }
